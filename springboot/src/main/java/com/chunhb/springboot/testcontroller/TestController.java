1a4a99d6dbe43d5ce7716b17b07790207c9ff8a6
huochaiyiersan <1179757089@qq.com>
2018-06-28 22:42:24 +0800

    新加logback日志功能

--- springboot/src/main/java/com/chunhb/springboot/testcontroller/TestController.java
+++ springboot/src/main/java/com/chunhb/springboot/testcontroller/TestController.java
@@ -1,11 +1,15 @@
 package com.chunhb.springboot.testcontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
     @GetMapping(value = "testString")
     public String testString(){
         return "test";
@@ -14,6 +18,8 @@ public class TestController {
     @GetMapping(value = "testJson")
     public String testJson(){
 
+        logger.info("info","atestjson_info");
+        logger.error("error","atestjson_error");
         return "json";
     }
 
