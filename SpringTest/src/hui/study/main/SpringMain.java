f60b208462066dcdf78f053a328ed843ba98ab7d
chunhb <@travelsky.com>
2019-02-28 15:51:45 +0800

    注入对象

--- SpringTest/src/hui/study/main/SpringMain.java
+++ SpringTest/src/hui/study/main/SpringMain.java
@@ -3,6 +3,7 @@ package hui.study.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import hui.study.model.School;
 import hui.study.model.Student;
 
 public class SpringMain {
@@ -13,13 +14,20 @@ public class SpringMain {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("sources\\bean.xml");
         //此处的helloWorld对应Beans.xml中配置的<bean>标签的id
-        Student obj = (Student) context.getBean("helloWorld");
-       System.out.println( obj.getStuName());
+        Student obj = (Student) context.getBean("student");
+        System.out.println( obj.getStuName());
 
         //传统方式，需要对象的时候new一个对象
         Student hw = new Student();
         hw.setStuName("ni hao");
         System.out.println(hw.getStuName());
+        
+        
+        //获取学校类，通过xml配置注入学生类到学校类中
+        
+        School school = (School)context.getBean("school");
+        
+        System.out.println("学校类："+school.getSchname()+"学生类："+school.getS_student().getStuName());
 
 
     }}
