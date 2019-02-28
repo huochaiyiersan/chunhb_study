a85d552412b09a42a53c2b99ad8aae50530aa592
chunhb <@travelsky.com>
2019-02-28 16:18:35 +0800

    注入

---
+++ SpringTest/src/hui/study/model/School.java
@@ -0,0 +1,29 @@
+package hui.study.model;
+
+/**
+ * 采用在xml中配置注入一个学生类
+ * @author chunhb
+ *
+ */
+public class School {
+	private String schname;
+	private Student s_student;
+
+
+	public String getSchname() {
+		return schname;
+	}
+	public void setSchname(String schname) {
+		this.schname = schname;
+	}
+	public Student getS_student() {
+		return s_student;
+	}
+	public void setS_student(Student s_student) {
+		this.s_student = s_student;
+	}
+	
+	
+	
+
+}
